8378c22cadaf2cc826acc7e07dbffef920e8721c
Shatha Makhlouf <shada_m2006@yahoo.com>
2020-12-03 00:40:12 +0200

    Add checkValidDate to inputChecker

--- src/client/js/inputChecker.js
+++ src/client/js/inputChecker.js
@@ -1,3 +1,22 @@
+// check the date of return (return date is valid) is after date of departure
+const checkValidDate = (input) => {
+
+    // $('.date').on('change', function () {
+    // console.log(new Date(this.value));
+
+    if (input.return_date < input.departure_date) {
+        alert("Invalid return date");
+        // console.log(false);
+        return false;
+    } else {
+        console.log(true);
+        return true;
+    }
+
+    // });
+}
+
+
 function checkForInput(input) {
     // Checks for input fields for data
     for (let key in input) {
@@ -8,6 +27,9 @@ function checkForInput(input) {
             return false;
         }
     }
-    return true;
+    return checkValidDate(input);
 }
+
+
+
 export { checkForInput }
