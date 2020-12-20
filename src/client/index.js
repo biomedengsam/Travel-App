b91d7f327161420185e9943ac89cc6fb3cd1fb2e
Shatha Makhlouf <shada_m2006@yahoo.com>
2020-12-20 20:04:48 +0200

    feat: Add delete trip button

--- src/client/index.js
+++ src/client/index.js
@@ -2,6 +2,7 @@ import { checkForInput } from './js/inputChecker'
 import { handleSubmit } from './js/formHandler'
 import { enterApp } from './js/eventListener'
 import { appinfo } from './js/app'
+import { deleteTrip } from './js/eventListener'
 import './styles/resets.scss'
 import './styles/landing.scss'
 import './styles/functions.scss'
@@ -9,7 +10,11 @@ import './styles/base.scss'
 import './styles/footer.scss'
 import './styles/form.scss'
 import './styles/header.scss'
+import './styles/trips.scss'
 import bg from './media/bg.jpg';
+import flight from './media/flight.jpg';
+import flying from './media/flying.jpg';
+import plane from './media/plane.jpg'
 import $ from "jquery";
 // import boardN from './media/boardN.jpg';
 // import boardNeg from './media/boardNeg.jpg';
@@ -20,12 +25,14 @@ let logoImg = document.getElementById('logo');
 logoImg.src = logo;
 let bgImg = document.getElementById('bg');
 bgImg.src = bg;
+// let Img = document.getElementById('img');
+// Img.src = plane;
 console.log("CHANGE!!");
 // event listener on entering app
 $('#enter').on('click', enterApp)
 // event listener on submitting form
 $('form').on('submit', handleSubmit);
-
+// $('#delete').on('click', deleteTrip);
 
 
 export {
@@ -34,5 +41,6 @@ export {
     handleSubmit,
     enterApp,
     datepicker,
-    appinfo
+    appinfo,
+    deleteTrip
 }
