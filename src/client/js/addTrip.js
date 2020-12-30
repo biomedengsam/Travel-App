bbd84dc5dea96d8d69ead4345e4cdbdd0e539e1a
Shatha Makhlouf <shada_m2006@yahoo.com>
2020-12-30 17:11:51 +0200

    docs:Update readme file

--- src/client/js/addTrip.js
+++ src/client/js/addTrip.js
@@ -1,5 +1,4 @@
-import $ from "jquery";
-
+// Add new trip
 const trip = (info) => {
     // Get data from local storage
     let tripsArray = JSON.parse(localStorage.getItem('trips'));
@@ -22,54 +21,54 @@ const trip = (info) => {
     let languages = lang.toString()
 
     //Switch visibility on for the title of my trips section.And delete all trips button
-    $(".trips-title").css("visibility", "visible");
-    $(".delete-all-trips").css("visibility", "visible");
+    document.querySelector(".trips-title").style.visibility = "visible";
+    document.getElementById("delete-all-trips").style.visibility = "visible";
 
     let content =
-        ` <div class="card" >
-              <img src="${info.imageUrl}" class="card-img-top countryImg" alt="photo of ${info.country}">
-              <div class="card-body">
-                <div class="row">
-                  <div class="col-sm-8">
-                    <img id="flag" src=" ${info.flagUrl}" alt="flag of ${info.country}>
-                    <h2 class="card-title "><strong>${info.destination} /${info.country}</strong></h2><hr>
-                    <div class="info">
-                        <div>
-                            <p>Destination:  ${info.destination}</p>
-                            <p>Country:  ${info.country}</p>
-                            <p>Region : ${info.region}</p>
-                            <p>Capital : ${info.capital}</p>
-                            <p>Population : ${info.population}</p>
-                            <p>Languages : ${languages}</ >
-                            <p>Currencies : ${currencies}</ >
-                        </div>
-                        <div>
-                            <p>Departure Date:${info.departure}</p>
-                            <p>Return Date:${info.return}</p>
-                            <p> ${info.remainingDays} Days left for your ${info.tripDuration} day trip to ${info.destination}</p>
-                             <a target="_blank" href="https://en.wikivoyage.org/wiki/${info.country}" id="more-info" class="btn btn-primary">For more info about ${info.country}</a>
-                        </div>
+        `<img src="${info.imageUrl}" class="card-img-top countryImg" alt="photo of ${info.country}">
+            <div class="card-body">
+            <div class="row">
+                <div class="col-sm-8">
+                <img id="flag" src=" ${info.flagUrl}" alt="flag of ${info.country}>
+                <h2 class="card-title "><strong>${info.destination} /${info.country}</strong></h2><hr>
+                <div class="info">
+                    <div>
+                        <p>Destination:  ${info.destination}</p>
+                        <p>Country:  ${info.country}</p>
+                        <p>Region : ${info.region}</p>
+                        <p>Capital : ${info.capital}</p>
+                        <p>Population : ${info.population}</p>
+                        <p>Languages : ${languages}</ >
+                        <p>Currencies : ${currencies}</ >
                     </div>
-                    <button  type="button" name="button" data-tripIndex="${tripIndex}"  class="btn btn-danger delete" onclick="Client.deleteTrip(this)" >Delete Trip</button>
-                  </div >
-                    <div class="col-sm-4 weather">
-                    <h3 id="weather-title">weather on trip date</h3>
-                    <h6 class="weather-info">Description</h6>
-                    <p>${info.weather.weatherInfo.description}</p>
-                    <img class="weather-icon" src="https://www.weatherbit.io/static/img/icons/${info.weather.weatherInfo.icon}.png" alt="weather icon">
-                    <h6 class="weather-info">Temperature</h6>
-                    <p>${info.weather.temp}<P>
+                    <div>
+                        <p>Departure Date:${info.departure}</p>
+                        <p>Return Date:${info.return}</p>
+                        <p> ${info.remainingDays} Days left for your ${info.tripDuration} day trip to ${info.destination}</p>
+                            <a target="_blank" href="https://en.wikivoyage.org/wiki/${info.country}" id="more-info" class="btn btn-primary">For more info about ${info.country}</a>
                     </div>
-                    </div >
+                </div>
+                <button  type="button" name="button" data-tripIndex="${tripIndex}"  class="btn btn-danger delete" onclick="Client.deleteTrip(this)" >Delete Trip</button>
                 </div >
-                </div > `
+                <div class="col-sm-4 weather">
+                <h3 id="weather-title">weather on trip date</h3>
+                <h6 class="weather-info">Description</h6>
+                <p>${info.weather.weatherInfo.description}</p>
+                <img class="weather-icon" src="https://www.weatherbit.io/static/img/icons/${info.weather.weatherInfo.icon}.png" alt="weather icon">
+                <h6 class="weather-info">Temperature</h6>
+                <p>${info.weather.temp}<P>
+                </div>
+                </div >
+            </div >`
 
-    // Append trip as first child
-    $('#myTrips').prepend(content);
+    let trip = document.createElement('div');
+    trip.setAttribute('class', 'card');
+    trip.innerHTML = content;
+    document.getElementById('myTrips').appendChild(trip);
 
-    // Scroll to my trips smoothly
+    // Scroll to new trip smoothly
     let scrollTO = () => {
-        $('#myTrips')[0].scrollIntoView({ behavior: 'smooth' });
+        document.getElementById('myTrips').lastChild.scrollIntoView({ behavior: 'smooth' });
     }
     scrollTO();
 }
